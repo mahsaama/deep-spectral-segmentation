f49adb18fc30feca3cfd7036c76bacc9766861b9
mahsa.ama <mahsa.ama1391@gmail.com>
2022-06-04 10:55:56 +0430

    debug

--- extract/extract_utils.py
+++ extract/extract_utils.py
@@ -57,7 +57,7 @@ def get_transform(name: str):
     if any(x in name for x in ('dino', 'mocov3', 'convnext', )):
         # normalize = transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
         # normalize = tio.ZNormalization()
-        transform = transforms.Compose([transforms.ToTensor(), normalize])
+        transform = transforms.Compose([transforms.ToTensor()])
     else:
         raise NotImplementedError()
     return transform
